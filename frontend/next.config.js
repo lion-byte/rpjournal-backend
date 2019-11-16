7d0da896a327f86d372854f9321e112594cbf3d4
Mark Hernandez <mark@lion-byte.com>
2019-11-16 17:54:09 -0600

    chore: drop frontend codebase
    The frontend code has been moved to a separate repo.

--- frontend/next.config.js
+++
@@ -1,5 +0,0 @@
-const withCSS = require('@zeit/next-css')
-const withFonts = require('next-fonts')
-const withSourceMaps = require('@zeit/next-source-maps')
-
-module.exports = withSourceMaps(withCSS(withFonts()))
